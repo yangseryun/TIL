5a9e0b76bb096f4ab0fb3cb9409f2d1259102607
yangseryun <did6504@naver.com>
2021-09-21 13:54:36 +0900

    210921

---
+++ JAVA/day16/src/day16/VariableTest.java
@@ -0,0 +1,14 @@
+package day16;
+
+public class VariableTest {
+	int data = 0;
+	
+	void f() {
+		System.out.println(++data);
+	}
+	
+	void f2() {
+		data=20;
+	}
+	
+}
---
+++ JAVA/day16/src/day16/VariableTest2.java
@@ -0,0 +1,18 @@
+package day16;
+
+public class VariableTest2 {
+	public static void main(String[] args) {
+		VariableTest vt = new VariableTest();
+		vt.f();
+		vt.f();
+		vt.f();
+		vt.f();
+		vt.f();
+		
+		vt.f2();
+		vt.f();
+		
+		vt = new VariableTest();
+		vt.f();
+	}
+}
